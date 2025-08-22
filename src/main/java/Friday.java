2ec4e0b6431d2984ee9b88eec1a5b00dad90e32b
goodguykf <kuanfengtan@gmail.com>
2025-08-22 15:44:37 +0800

    Initial Commit to initialise chatbot
    This commit changes renamed the chatbot and also at
    the same time allows the chatbot to greet its user.

---
+++ src/main/java/Friday.java
@@ -0,0 +1,9 @@
+public class Friday {
+    public static void main(String[] args) {
+
+        System.out.println("\nHello! I'm Friday"); //Basic chatbot
+        System.out.println("What can I do for you?\n");
+        System.out.println("Bye. Hope to see you again soon!");
+
+    }
+}
