715cc0916685365e70b65882239c774fc1514e9e
goodguykf <kuanfengtan@gmail.com>
2025-08-22 16:30:29 +0800

    Add and List
    I have added a fixed String array to collect task sent by user
    and the ability to reprint all the lists.

--- src/main/java/Friday.java
+++ src/main/java/Friday.java
@@ -1,20 +1,30 @@
-import java.util.Scanner;
+import java.util.*;
 
 public class Friday {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        int pointer = 1; //Pointer to keep track of where to store incoming task
+        String[] list = new String[100]; //List to store tasks
+
         System.out.println("\nHello! I'm Friday"); //Message sent when the bot is activated
         System.out.println("What can I do for you?");
 
-        //to keep the system running
+
+        //To keep the system running
         while (true) {
             String input = sc.nextLine();
 
             if(input.equals("bye")) {
-                System.out.println("\n  Bye. Hope to see you again soon!");
+                System.out.println("\n  Bye. Hope to see you again soon!"); //notify user that bot is turning off
                 break;
+            } else if(input.equals("list")) {
+                for(int i = 1; i < pointer; i++) { //print out task recursively
+                    System.out.println("    " + i + ". " + list[i]);
+                }
             } else {
-                System.out.println("\n  " + input + "\n");
+                list[pointer] = input;
+                pointer++;
+                System.out.println("added: " + input); // to notify the user that task is added
             }
         }
 
