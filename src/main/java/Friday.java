a08420b024f431fdfe28156bc6a5ae81e5c683b8
goodguykf <kuanfengtan@gmail.com>
2025-08-22 16:05:48 +0800

    Upgrading Skeletal structure
    Improved the skeleton structure to add feedback capabilities

--- src/main/java/Friday.java
+++ src/main/java/Friday.java
@@ -1,9 +1,22 @@
+import java.util.Scanner;
+
 public class Friday {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("\nHello! I'm Friday"); //Message sent when the bot is activated
+        System.out.println("What can I do for you?");
 
-        System.out.println("\nHello! I'm Friday"); //Basic chatbot
-        System.out.println("What can I do for you?\n");
-        System.out.println("Bye. Hope to see you again soon!");
+        //to keep the system running
+        while (true) {
+            String input = sc.nextLine();
+
+            if(input.equals("bye")) {
+                System.out.println("\n  Bye. Hope to see you again soon!");
+                break;
+            } else {
+                System.out.println("\n  " + input + "\n");
+            }
+        }
 
     }
 }
